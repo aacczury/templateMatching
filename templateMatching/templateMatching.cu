50a5f2ac21ab9518916417552c349feb53e62bdf
aacczury <yl931905@gmail.com>
2015-06-07 20:50:09 +0800

    success gpu matchTemplate

--- templateMatching/templateMatching.cu
+++ templateMatching/templateMatching.cu
@@ -1,15 +1,91 @@
 #include <iostream>
+#include <Windows.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/gpu/gpu.hpp>
+#include <opencv2/gpu/stream_accessor.hpp>
+typedef unsigned int uint;
+#include <opencv2/gpu/device/vec_traits.hpp>
 
-#include <cuda.h>
-#include <device_launch_parameters.h>
 #include <cuda_runtime.h>
 
+
 using namespace cv;
+using namespace cv::gpu;
 using namespace std;
+
+__device__ __forceinline__ float sum(float v) { return v; }
+__device__ __forceinline__ float sum(float2 v) { return v.x + v.y; }
+__device__ __forceinline__ float sum(float3 v) { return v.x + v.y + v.z; }
+__device__ __forceinline__ float sum(float4 v) { return v.x + v.y + v.z + v.w; }
+
+__device__ __forceinline__ float add(float a, float b) { return a + b; }
+__device__ __forceinline__ float2 add(float2 a, float2 b) { return make_float2(a.x + b.x, a.y + b.y); }
+__device__ __forceinline__ float3 add(float3 a, float3 b) { return make_float3(a.x + b.x, a.y + b.y, a.z + b.z); }
+__device__ __forceinline__ float4 add(float4 a, float4 b) { return make_float4(a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w); }
+
+__device__ __forceinline__ float mul(float a, float b) { return a * b; }
+__device__ __forceinline__ float2 mul(float2 a, float2 b) { return make_float2(a.x * b.x, a.y * b.y); }
+__device__ __forceinline__ float3 mul(float3 a, float3 b) { return make_float3(a.x * b.x, a.y * b.y, a.z * b.z); }
+__device__ __forceinline__ float4 mul(float4 a, float4 b) { return make_float4(a.x * b.x, a.y * b.y, a.z * b.z, a.w * b.w); }
+
+__device__ __forceinline__ float sub(float a, float b) { return a - b; }
+__device__ __forceinline__ float2 sub(float2 a, float2 b) { return make_float2(a.x - b.x, a.y - b.y); }
+__device__ __forceinline__ float3 sub(float3 a, float3 b) { return make_float3(a.x - b.x, a.y - b.y, a.z - b.z); }
+__device__ __forceinline__ float4 sub(float4 a, float4 b) { return make_float4(a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w); }
+
+template <typename T, int cn>
+__global__ void matchTemplateNaiveKernel_SQDIFF(int w, int h, const PtrStepb image, const PtrStepb templ, PtrStepSzf result){
+	typedef typename device::TypeVec<T, cn>::vec_type Type;
+	typedef typename device::TypeVec<float, cn>::vec_type Typef;
+
+	int x = blockDim.x * blockIdx.x + threadIdx.x;
+	int y = blockDim.y * blockIdx.y + threadIdx.y;
+
+	if (x < result.cols && y < result.rows){
+		Typef res = device::VecTraits<Typef>::all(0);
+		Typef delta;
+
+		for (int i = 0; i < h; ++i){
+			const Type* image_ptr = (const Type*)image.ptr(y + i);
+			const Type* templ_ptr = (const Type*)templ.ptr(i);
+			for (int j = 0; j < w; ++j){
+				delta = sub(image_ptr[x + j], templ_ptr[j]);
+				res = add(res, mul(delta, delta));
+			}
+		}
+
+		result.ptr(y)[x] = sum(res);
+	}
+}
+
+template <typename T, int cn>
+void matchTemplateNaive_SQDIFF(const PtrStepSzb image, const PtrStepSzb templ, PtrStepSzf result, cudaStream_t stream){
+	const dim3 threads(32, 8);
+	const dim3 grid(divUp(result.cols, threads.x), divUp(result.rows, threads.y));
+
+	matchTemplateNaiveKernel_SQDIFF<T, cn> << <grid, threads, 0, stream >> >(templ.cols, templ.rows, image, templ, result);
+	cudaSafeCall(cudaGetLastError());
+
+	if (stream == 0)
+		cudaSafeCall(cudaDeviceSynchronize());
+}
+
+void matchTemplateNaive_SQDIFF_32F(const PtrStepSzb image, const PtrStepSzb templ, PtrStepSzf result, int cn, cudaStream_t stream){
+	typedef void(*caller_t)(const PtrStepSzb image, const PtrStepSzb templ, PtrStepSzf result, cudaStream_t stream);
+
+	static const caller_t callers[] = {
+		0, matchTemplateNaive_SQDIFF<float, 1>, matchTemplateNaive_SQDIFF<float, 2>, matchTemplateNaive_SQDIFF<float, 3>, matchTemplateNaive_SQDIFF<float, 4>
+	};
+
+	callers[cn](image, templ, result, stream);
+}
+
+void matchTemplate_SQDIFF_32F(const GpuMat& image, const GpuMat& templ, GpuMat& result, Stream& stream){
+	result.create(image.rows - templ.rows + 1, image.cols - templ.cols + 1, CV_32F);
+	matchTemplateNaive_SQDIFF_32F(image, templ, result, image.channels(), StreamAccessor::getStream(stream));
+}
 
 typedef struct tmpMatchDiff{
 	char *filename;
@@ -17,114 +93,53 @@ typedef struct tmpMatchDiff{
 	float diff;
 }matchDiff;
 
-void VectorAdd(Mat inputImg, Mat &resultImg, Mat templImg, Mat Mask){
-	//int i = threadIdx.x;
-	int result_cols = inputImg.cols - templImg.cols + 1;
-	int result_rows = inputImg.rows - templImg.rows + 1;
-
-
-	for (int j = 0; j < result_rows; ++j){
-		for (int i = 0; i < result_cols; ++i){
-			Mat tmpI(inputImg, Rect(i, j, templImg.cols, templImg.rows)), I;
-			tmpI.copyTo(I, Mask); tmpI.release();
-			((float *)resultImg.data)[j * result_cols + i] = (float)norm(sum((I-templImg).mul(I-templImg)));
-			I.release();
-		}
-	}
-}
-
-void templateMatching(Mat inputImg, Mat &resultImg, Mat templImg, Mat Mask, vector<matchDiff> &allDiff, char *fileWithDir){
-	/// Create the result matrix
-	int result_cols = inputImg.cols - templImg.cols + 1;
-	int result_rows = inputImg.rows - templImg.rows + 1;
-
-	resultImg.create(result_rows, result_cols, CV_32FC1);
-
-	gpu::GpuMat d_inputImg(inputImg), d_templImg(templImg), d_Mask(Mask), d_resultImg(resultImg);
-	for (int j = 0; j < result_rows; ++j){
-		for (int i = 0; i < result_cols; ++i){
-			gpu::GpuMat tmpI(d_inputImg, Rect(i, j, templImg.cols, templImg.rows)), d_I;
-			tmpI.copyTo(d_I, d_Mask); tmpI.release();
-			gpu::GpuMat d_disMat;
-			gpu::subtract(d_I, d_templImg, d_disMat);
-			gpu::multiply(d_disMat, d_disMat, d_disMat);
-			(float)gpu::norm(d_disMat);
-			d_I.release();
-		}
-		//printf("%d\n", j);
-	}
-
-	for (int j = 0; j < resultImg.rows; ++j)
-		for (int i = 0; i < resultImg.cols; ++i){
-			matchDiff pDiff;
-			pDiff.filename = fileWithDir;
-			pDiff.pos = Vec2i(i, j); // (col, row) (x, y)
-			pDiff.diff = ((float *)resultImg.data)[j * resultImg.cols + i];
-			allDiff.push_back(pDiff);
-		}
-	//cout << "finish" << endl;
-	normalize(resultImg, resultImg, 0, 1, NORM_MINMAX, -1, Mat());
-	resultImg.convertTo(resultImg, CV_8UC1, 255.0);
-
-	return;
-}
-
 int main(int argc, char* argv[])
 {
-	printf("Device count: %i\n", cv::gpu::getCudaEnabledDeviceCount());
-	cv::gpu::DeviceInfo dev_info(0);
+	printf("Device count: %i\n", getCudaEnabledDeviceCount());
+	DeviceInfo dev_info(0);
 	printf("Device compatible: %d\n", dev_info.isCompatible());
 
-	try
-	{
-		/*Mat src_host = imread("input.png", CV_LOAD_IMAGE_GRAYSCALE);
-		gpu::GpuMat dst, src(src_host);
+	Mat img, outputImg;
+	double scale = 0.2;
+	vector<matchDiff> allDiff;
+	img = imread("input.png", CV_LOAD_IMAGE_UNCHANGED);
 
-		gpu::threshold(src, dst, 128.0, 255.0, CV_THRESH_BINARY);
+	Mat templ;
+	Mat resizeImg, resizeTempl; // resize + rgba
+	Mat splitImg[4], splitTempl[4]; // resize + r,g,b,a
+	Mat rgbImg, rgbTempl; // resize + rgb
+	char *inputDir = "match", *outputDir = "result";
+	allDiff.clear();
 
-		Mat result_host(dst);
-		imshow("Result", result_host);*/
+	resize(img, resizeImg, Size(img.cols * scale, img.rows * scale));
+	split(resizeImg, splitImg);
+	merge(splitImg, 3, rgbImg);
+	rgbImg.convertTo(rgbImg, CV_32FC4, 1.0 / 255.0);
+	templ = imread("match.png", CV_LOAD_IMAGE_UNCHANGED);
+	resize(templ, resizeTempl, Size(templ.cols * scale, templ.rows * scale));
+	split(resizeTempl, splitTempl); // alpha channel is mask would be CV_8U
+	merge(splitTempl, 3, rgbTempl);
+	rgbTempl.convertTo(rgbTempl, CV_32FC4, 1.0 / 255.0);
 
+	LARGE_INTEGER startTime, endTime, fre;
+	QueryPerformanceFrequency(&fre); // 取得CPU頻率
+	QueryPerformanceCounter(&startTime); // 取得開機到現在經過幾個CPU Cycle
 
-		Mat img, outputImg;
-		double scale = 0.2;
-		vector<matchDiff> allDiff;
-		img = imread("input.png", CV_LOAD_IMAGE_UNCHANGED);
+	GpuMat d_rgbImg(rgbImg), d_resultImg, d_rgbTempl(rgbTempl);
+	matchTemplate_SQDIFF_32F(d_rgbImg, d_rgbTempl, d_resultImg, Stream::Null());
 
-		Mat templ;
-		Mat resizeImg, resizeTempl; // resize + rgba
-		Mat splitImg[4], splitTempl[4]; // resize + r,g,b,a
-		Mat rgbImg, rgbTempl; // resize + rgb
-		char *inputDir = "match", *outputDir = "result";
-		allDiff.clear();
+	Mat resultImg;
+	d_resultImg.download(resultImg);
+	normalize(resultImg, resultImg, 0, 1, NORM_MINMAX, -1, Mat());
 
-		resize(img, resizeImg, Size(img.cols * scale, img.rows * scale));
-		split(resizeImg, splitImg);
-		merge(splitImg, 3, rgbImg);
-		rgbImg.convertTo(rgbImg, CV_32FC4, 1.0 / 255.0);
-		templ = imread("match.png", CV_LOAD_IMAGE_UNCHANGED);
-		resize(templ, resizeTempl, Size(templ.cols * scale, templ.rows * scale));
-		split(resizeTempl, splitTempl); // alpha channel is mask would be CV_8U
-		merge(splitTempl, 3, rgbTempl);
-		rgbTempl.convertTo(rgbTempl, CV_32FC4, 1.0 / 255.0);
+	QueryPerformanceCounter(&endTime); //取得開機到程式執行完成經過幾個CPU Cycle
+	printf("Elapsed Time: %lf", ((double)endTime.QuadPart - (double)startTime.QuadPart) / fre.QuadPart);
 
-		Mat resultImg;
+	namedWindow("output", CV_WINDOW_AUTOSIZE);
+	imshow("output", resultImg);
+	imwrite("outputImg.png", resultImg);
+	waitKey(0);
 
-		templateMatching(rgbImg, resultImg, rgbTempl, splitTempl[3], allDiff, "match.png");
-		//runTemplateMatching(img, allDiff, scale);
-		//outputDiff(allDiff);
-		//outputByTxt(img, outputImg, scale);
-
-		namedWindow("output", CV_WINDOW_AUTOSIZE);
-		imshow("output", resultImg);
-		imwrite("outputImg.png", resultImg);
-	}
-	catch (const cv::Exception& ex)
-	{
-		cout << "Error: " << ex.what() << endl;
-	}
-
-	cv::waitKey(0);
 	return 0;
 }
 
