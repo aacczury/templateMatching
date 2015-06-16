3026f6172c2f43be9462326aa0e0b5ad8f533859
aacczury <yl931905@gmail.com>
2015-06-16 18:56:40 +0800

    add gpu multi match file

--- templateMatching/templateMatching.cu
+++ templateMatching/templateMatching.cu
@@ -1,4 +1,6 @@
 #include <Windows.h>
+#include <iostream>
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -9,9 +11,11 @@
 #include <cuda_runtime.h>
 
 
+
 using namespace cv;
 using namespace cv::gpu;
 using namespace std;
+
 
 __device__ __forceinline__ float sum(float v) { return v; }
 __device__ __forceinline__ float sum(float2 v) { return v.x + v.y; }
@@ -93,22 +97,14 @@ typedef struct tmpMatchDiff{
 	float diff;
 }matchDiff;
 
-int main(int argc, char* argv[])
-{
-	printf("Device count: %i\n", getCudaEnabledDeviceCount());
-	DeviceInfo dev_info(0);
-	printf("Device compatible: %d\n", dev_info.isCompatible());
 
-	Mat img, outputImg;
-	double scale = 0.2;
-	vector<matchDiff> allDiff;
-	img = imread("input.png", CV_LOAD_IMAGE_UNCHANGED);
-	outputImg = img.clone();
-
+bool compareByDiff(const matchDiff &a, const matchDiff &b){
+	return a.diff < b.diff;
+}
+void runTemplateMatching(Mat img, vector<matchDiff> &allDiff, double scale){
 	Mat templ;
 	Mat resizeImg, resizeTempl; // resize + rgba
 	Mat splitImg[4], splitTempl[4]; // resize + r,g,b,a
-	Mat oSplitTempl[4];
 	Mat rgbImg, rgbTempl; // resize + rgb
 	char *inputDir = "match", *outputDir = "result";
 	allDiff.clear();
@@ -117,37 +113,125 @@ int main(int argc, char* argv[])
 	split(resizeImg, splitImg);
 	merge(splitImg, 3, rgbImg);
 	rgbImg.convertTo(rgbImg, CV_32FC4, 1.0 / 255.0);
-	templ = imread("match.png", CV_LOAD_IMAGE_UNCHANGED);
-	split(templ, oSplitTempl);
-	resize(templ, resizeTempl, Size(templ.cols * scale, templ.rows * scale));
-	split(resizeTempl, splitTempl); // alpha channel is mask would be CV_8U
-	merge(splitTempl, 3, rgbTempl);
-	rgbTempl.convertTo(rgbTempl, CV_32FC4, 1.0 / 255.0);
-	splitTempl[3].convertTo(splitTempl[3], CV_32FC1, 1.0 / 255.0);
+	GpuMat d_rgbImg(rgbImg);
+
+	char systemStr[100];
+	sprintf(systemStr, "dir /B %s > matchDir.txt", inputDir);
+	system(systemStr);
+	FILE *file = fopen("matchDir.txt", "r");
+	char fileName[100];
+	while (fgets(fileName, 100, file)){
+		strtok(fileName, "\n");
+		char *fileWithDir = (char *)malloc(100 * sizeof(char));
+		sprintf(fileWithDir, "%s\\%s", inputDir, fileName);
+		printf("%s\n", fileName);
+
+		templ = imread(fileWithDir, CV_LOAD_IMAGE_UNCHANGED);
+		resize(templ, resizeTempl, Size(templ.cols * scale, templ.rows * scale));
+		resizeTempl.convertTo(resizeTempl, CV_32FC4, 1.0 / 255.0);
+		split(resizeTempl, splitTempl);
+		merge(splitTempl, 3, rgbTempl);
+
+		GpuMat d_resultImg, d_rgbTempl(rgbTempl), d_mask(splitTempl[3]);
+		matchTemplate_SQDIFF_32F(d_rgbImg, d_rgbTempl, d_resultImg, d_mask);
+
+		Mat resultImg;
+		d_resultImg.download(resultImg);
+
+		for (int x = 0; x < resultImg.cols; ++x){
+			for (int y = 0; y < resultImg.rows; ++y){
+				matchDiff pDiff;
+				pDiff.filename = fileWithDir;
+				pDiff.pos = Vec2i(x, y); // (col, row) (x, y)
+				pDiff.diff = ((float *)resultImg.data)[y * resultImg.cols + x];
+				allDiff.push_back(pDiff);
+			}
+		}
+	}
+	fclose(file);
+	system("rm matchDir.txt");
+	sort(allDiff.begin(), allDiff.end(), compareByDiff);
+}
+
+void outputDiff(vector<matchDiff> allDiff){
+	FILE *pFile;
+	pFile = fopen("outAllDiff.txt", "w");
+	for (int i = 0; i < allDiff.size(); ++i)
+		fprintf(pFile, "%s,%d,%d,%f\n", allDiff[i].filename, allDiff[i].pos[0], allDiff[i].pos[1], allDiff[i].diff);
+	fclose(pFile);
+}
+
+void outputByTxt(Mat img, Mat &outputImg, double scale){
+	img.copyTo(outputImg);
+	map<string, int>fileNameHash;
+
+	FILE *pFile;
+	pFile = fopen("outAllDiff.txt", "r");
+	char str[1000];
+	int count = 0;
+	vector<matchDiff> outputMatch; outputMatch.clear();
+	while (fgets(str, 1000, pFile)){
+		char *filename = (char *)malloc(100 * sizeof(char));
+		int x, y;
+		char *pch = strtok(str, ",");
+		strcpy(filename, pch);
+		pch = strtok(NULL, ",");
+		x = atoi(pch);
+		pch = strtok(NULL, ",");
+		y = atoi(pch);
+
+		string fileStr(filename);
+		if (fileNameHash.find(fileStr) == fileNameHash.end()){
+			//cout << fileStr << endl;
+			count++;
+			fileNameHash[fileStr] = 1;
+			matchDiff pDiff; pDiff.filename = filename; pDiff.pos = Vec2i(x, y);
+			outputMatch.push_back(pDiff);
+		}
+		if (count >= 100)
+			break;
+	}
+
+	Mat templ, oSplitTempl[4];
+	for (int i = outputMatch.size() - 1; i >= 0; --i){
+		templ = imread(outputMatch[i].filename, CV_LOAD_IMAGE_UNCHANGED);
+		split(templ, oSplitTempl);
+		templ.copyTo(outputImg(Rect(outputMatch[i].pos[0] / scale, outputMatch[i].pos[1] / scale, templ.cols, templ.rows)), oSplitTempl[3]);
+	}
+}
+
+int main(int argc, char* argv[])
+{
+
+	// test gpu support
+	printf("Device count: %i\n", getCudaEnabledDeviceCount());
+	DeviceInfo dev_info(0);
+	printf("Device compatible: %d\n", dev_info.isCompatible());
+
+	Mat img, outputImg;
+	double scale = 0.2;
+	vector<matchDiff> allDiff;
+
+	img = imread("input.png", CV_LOAD_IMAGE_UNCHANGED);
+
 
 	LARGE_INTEGER startTime, endTime, fre;
 	QueryPerformanceFrequency(&fre); // 取得CPU頻率
 	QueryPerformanceCounter(&startTime); // 取得開機到現在經過幾個CPU Cycle
-
-	GpuMat d_rgbImg(rgbImg), d_resultImg, d_rgbTempl(rgbTempl), d_mask(splitTempl[3]);
-	matchTemplate_SQDIFF_32F(d_rgbImg, d_rgbTempl, d_resultImg, d_mask);
-
-	Mat resultImg;
-	d_resultImg.download(resultImg);
-	//normalize(resultImg, resultImg, 0, 1, NORM_MINMAX, -1, Mat());
+	
+	runTemplateMatching(img, allDiff, scale);
 
 	QueryPerformanceCounter(&endTime); //取得開機到程式執行完成經過幾個CPU Cycle
-	printf("Elapsed Time: %lf", ((double)endTime.QuadPart - (double)startTime.QuadPart) / fre.QuadPart);
+	printf("Elapsed Time: %lf\n", ((double)endTime.QuadPart - (double)startTime.QuadPart) / fre.QuadPart);
 
-	double minVal; double maxVal; Point minLoc; Point maxLoc;
-	minMaxLoc(resultImg, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
-	templ.copyTo(outputImg(Rect(minLoc.x / scale, minLoc.y / scale, templ.cols, templ.rows)), oSplitTempl[3]);
+	outputDiff(allDiff);
+	outputByTxt(img, outputImg, scale);
 
 	namedWindow("output", CV_WINDOW_AUTOSIZE);
 	imshow("output", outputImg);
-	imwrite("outputImg.png", resultImg);
-	waitKey(0);
+	imwrite("outputImg.png", outputImg);
 
+	waitKey(0);
 	return 0;
 }
 
