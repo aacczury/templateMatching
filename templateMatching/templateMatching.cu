5cd5f7ab1646177b89a533c4cc96f495723bdd13
aacczury <yl931905@gmail.com>
2015-06-21 16:54:44 +0800

    add output by obj

--- templateMatching/templateMatching.cu
+++ templateMatching/templateMatching.cu
@@ -93,13 +93,13 @@ void matchTemplate_SQDIFF_32F(const GpuMat& image, const GpuMat& templ, GpuMat& 
 
 typedef struct tmpMatchDiff{
 	char *filename;
-	Vec2i pos;
-	float diff;
+	short x, y; // int 會超過記憶體限制
+	float diff, scale;
 }matchDiff;
 
 
 bool compareByDiff(const matchDiff &a, const matchDiff &b){
-	return a.diff < b.diff;
+	return a.diff != b.diff ? a.diff < b.diff : a.scale > b.scale;
 }
 void runTemplateMatching(Mat img, vector<matchDiff> &allDiff, double scale){
 	Mat templ;
@@ -122,81 +122,109 @@ void runTemplateMatching(Mat img, vector<matchDiff> &allDiff, double scale){
 	char fileName[100];
 	while (fgets(fileName, 100, file)){
 		strtok(fileName, "\n");
-		char *fileWithDir = (char *)malloc(100 * sizeof(char));
+		char *fileWithDir = (char *)malloc(40 * sizeof(char));
 		sprintf(fileWithDir, "%s\\%s", inputDir, fileName);
 		printf("%s\n", fileName);
 
 		templ = imread(fileWithDir, CV_LOAD_IMAGE_UNCHANGED);
-		resize(templ, resizeTempl, Size(templ.cols * scale, templ.rows * scale));
-		resizeTempl.convertTo(resizeTempl, CV_32FC4, 1.0 / 255.0);
-		split(resizeTempl, splitTempl);
-		merge(splitTempl, 3, rgbTempl);
 
-		GpuMat d_resultImg, d_rgbTempl(rgbTempl), d_mask(splitTempl[3]);
-		matchTemplate_SQDIFF_32F(d_rgbImg, d_rgbTempl, d_resultImg, d_mask);
+		for (double s = 0.1; s <= scale ; s += 0.05){
+			resize(templ, resizeTempl, Size(templ.cols * s, templ.rows * s));
+			resizeTempl.convertTo(resizeTempl, CV_32FC4, 1.0 / 255.0);
+			split(resizeTempl, splitTempl);
+			merge(splitTempl, 3, rgbTempl);
 
-		Mat resultImg;
-		d_resultImg.download(resultImg);
+			GpuMat d_resultImg, d_rgbTempl(rgbTempl), d_mask(splitTempl[3]);
+			matchTemplate_SQDIFF_32F(d_rgbImg, d_rgbTempl, d_resultImg, d_mask);
 
-		for (int x = 0; x < resultImg.cols; ++x){
-			for (int y = 0; y < resultImg.rows; ++y){
-				matchDiff pDiff;
-				pDiff.filename = fileWithDir;
-				pDiff.pos = Vec2i(x, y); // (col, row) (x, y)
-				pDiff.diff = ((float *)resultImg.data)[y * resultImg.cols + x];
-				allDiff.push_back(pDiff);
+			Mat resultImg;
+			d_resultImg.download(resultImg);
+
+			for (int x = 0; x < resultImg.cols; ++x){
+				for (int y = 0; y < resultImg.rows; ++y){
+					matchDiff pDiff;
+					pDiff.filename = fileWithDir;
+					pDiff.x = (short)(x / scale);
+					pDiff.y = (short)(y / scale);
+					pDiff.diff = ((float *)resultImg.data)[y * resultImg.cols + x];
+					pDiff.scale = s / scale;
+					allDiff.push_back(pDiff);
+				}
 			}
 		}
 	}
 	fclose(file);
 	system("rm matchDir.txt");
-	sort(allDiff.begin(), allDiff.end(), compareByDiff);
+	printf("Matching complete !!\n");
 }
 
-void outputDiff(vector<matchDiff> allDiff){
+void outputDiff(vector<matchDiff> &allDiff, char *filename){
 	FILE *pFile;
-	pFile = fopen("outAllDiff.txt", "w");
+	pFile = fopen(filename, "w");
 	for (int i = 0; i < allDiff.size(); ++i)
-		fprintf(pFile, "%s,%d,%d,%f\n", allDiff[i].filename, allDiff[i].pos[0], allDiff[i].pos[1], allDiff[i].diff);
+		fprintf(pFile, "%s,%d,%d,%f,%f\n", allDiff[i].filename, allDiff[i].x, allDiff[i].y, allDiff[i].scale, allDiff[i].diff);
 	fclose(pFile);
+	printf("write all diff file\n");
+	allDiff.clear();
 }
 
-void outputByTxt(Mat img, Mat &outputImg, double scale){
+void outputByObj(Mat img, Mat &outputImg, double scale, vector<matchDiff> &allDiff){
+	img.copyTo(outputImg);
+	map<string, int>fileNameHash;
+
+	vector<matchDiff> outputMatch; outputMatch.clear();
+	for(int i = 0; i < allDiff.size() - 1 && outputMatch.size() < 100; ++ i){
+		string fileStr(allDiff[i].filename);
+		if (fileNameHash.find(fileStr) == fileNameHash.end()){
+			fileNameHash[fileStr] = 1;
+			matchDiff pDiff = allDiff[i];
+			outputMatch.push_back(pDiff);
+		}
+	}
+
+	Mat templ, mask[4];
+	for (int i = outputMatch.size() - 1; i >= 0; --i){
+		templ = imread(outputMatch[i].filename, CV_LOAD_IMAGE_UNCHANGED);
+		resize(templ, templ, Size(templ.cols * outputMatch[i].scale, templ.rows * outputMatch[i].scale));
+		split(templ, mask);
+		templ.copyTo(outputImg(Rect(outputMatch[i].x, outputMatch[i].y, templ.cols, templ.rows)), mask[3]);
+	}
+}
+
+void outputByTxt(Mat img, Mat &outputImg, double scale, char *filename){
 	img.copyTo(outputImg);
 	map<string, int>fileNameHash;
 
 	FILE *pFile;
-	pFile = fopen("outAllDiff.txt", "r");
-	char str[1000];
-	int count = 0;
+	pFile = fopen(filename, "r");
+	char str[200];
 	vector<matchDiff> outputMatch; outputMatch.clear();
-	while (fgets(str, 1000, pFile)){
-		char *filename = (char *)malloc(100 * sizeof(char));
-		int x, y;
-		char *pch = strtok(str, ",");
-		strcpy(filename, pch);
-		pch = strtok(NULL, ",");
-		x = atoi(pch);
-		pch = strtok(NULL, ",");
-		y = atoi(pch);
+	while (fgets(str, 200, pFile)){
+		char *filename = (char *)malloc(40 * sizeof(char));
+		short x, y;
+		double t_scale;
+		char *pch = strtok(str, ","); strcpy(filename, pch);
+		pch = strtok(NULL, ","); x = atoi(pch);
+		pch = strtok(NULL, ","); y = atoi(pch);
+		pch = strtok(NULL, ","); t_scale = atof(pch);
 
 		string fileStr(filename);
 		if (fileNameHash.find(fileStr) == fileNameHash.end()){
-			//cout << fileStr << endl;
-			count++;
 			fileNameHash[fileStr] = 1;
-			matchDiff pDiff; pDiff.filename = filename; pDiff.pos = Vec2i(x, y);
+			matchDiff pDiff; pDiff.filename = filename; pDiff.x = x; pDiff.y = y; pDiff.scale = t_scale;
 			outputMatch.push_back(pDiff);
 		}
-		if (count >= 100)
+		if (outputMatch.size() >= 100)
 			break;
 	}
+	fclose(pFile);
 
-	Mat templ, oSplitTempl[4];
+	Mat templ, mask[4];
 	for (int i = outputMatch.size() - 1; i >= 0; --i){
 		templ = imread(outputMatch[i].filename, CV_LOAD_IMAGE_UNCHANGED);
-		split(templ, oSplitTempl);
-		templ.copyTo(outputImg(Rect(outputMatch[i].pos[0] / scale, outputMatch[i].pos[1] / scale, templ.cols, templ.rows)), oSplitTempl[3]);
+		resize(templ, templ, Size(templ.cols * outputMatch[i].scale, templ.rows * outputMatch[i].scale));
+		split(templ, mask);
+		templ.copyTo(outputImg(Rect(outputMatch[i].x, outputMatch[i].y, templ.cols, templ.rows)), mask[3]);
 	}
 }
 
@@ -224,8 +252,13 @@ int main(int argc, char* argv[])
 	QueryPerformanceCounter(&endTime); //取得開機到程式執行完成經過幾個CPU Cycle
 	printf("Elapsed Time: %lf\n", ((double)endTime.QuadPart - (double)startTime.QuadPart) / fre.QuadPart);
 
-	outputDiff(allDiff);
-	outputByTxt(img, outputImg, scale);
+	sort(allDiff.begin(), allDiff.end(), compareByDiff);
+	printf("Sort by diff finish !! size: %d\n", allDiff.size());
+
+	outputByObj(img, outputImg, scale, allDiff);
+
+	//outputDiff(allDiff, "outAllDiff.txt");
+	//outputByTxt(img, outputImg, scale, "outAllDiff.txt");
 
 	namedWindow("output", CV_WINDOW_AUTOSIZE);
 	imshow("output", outputImg);
